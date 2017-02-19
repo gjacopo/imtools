8b9eec1d8f91d4caf255bed3b6d955cf0d779b93
Jacopo Grazzini <gjacopo@gmail.com>
2017-02-19 01:42:42 +0100

    updated internal html links

--- docs/change.sh
+++ docs/change.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
 
-echo  REPLACE FOLDER PATHS
-for d in *; 
-do
-    if ! [ -d $d ]; then 
-	continue;
-    fi
-    for file in */*html;
-    do
-	replace $file ../../$d/html/ ../$d/
-    done
-    break
-done
-exit
+# echo  REPLACE FOLDER PATHS
+# for d in *; 
+# do
+#     if ! [ -d $d ]; then 
+# 	continue;
+#     fi
+#     echo replacements for folder $d...
+#     for file in */*html;
+#     do
+# 	replace $file ../../$d/html/ ../$d/
+#     done
+# done
+
 echo
 echo  REPLACE FILENAMES
-for f in */*html;
+for f in */*.html;
 do
+    echo replacements for file $f...
     fname=`basename $f .html`
     Ufname=`echo $fname | tr /a-z/ /A-Z/`
     for file in */*html;
     do
 	replace $file $Ufname.html $fname.html
     done
-    break
 done
     
